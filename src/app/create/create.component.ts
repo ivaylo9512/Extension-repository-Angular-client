dbe59ea61cf98aa5e7f1c7786cb995126f77d4ed
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-09-29 23:01:59 +0300

    create logic change for creating extension

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
-import { FormControl, NgForm } from '@angular/forms'
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms'
 import { debounceTime } from 'rxjs/operators';
 import { ExtensionsService } from '../services/extensions.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { MouseWheelDirective } from '../helpers/mouse-wheel.directive';
+import { SubmitScrollDirective } from '../helpers/submit-scroll.directive';
 
 @Component({
   selector: 'app-create',
@@ -14,70 +14,68 @@ import { MouseWheelDirective } from '../helpers/mouse-wheel.directive';
 export class CreateComponent implements OnInit {
   @ViewChild('tagsInputElmnt') tagsInputElmnt: ElementRef
   @ViewChild('tagsContainer') tagsContainer: ElementRef
-  @ViewChild(MouseWheelDirective) wheelDirective: MouseWheelDirective
+  @ViewChild(SubmitScrollDirective) wheelDirective: SubmitScrollDirective
   @ViewChild('extensionSection') extensionSection: ElementRef
 
   nameInput: FormControl = new FormControl()
-  gitHubInput: FormControl = new FormControl()
-  versionInput: FormControl = new FormControl()
-  descriptionInput: FormControl = new FormControl()
+  githubInput: FormControl = new FormControl()
   tagsInput: FormControl = new FormControl()
-  
+
+  extensionForm : FormGroup
+  errors: FieldErrors
+  error: string
+
   formData: FormData
   logoURL: any
   coverURL: any
-  gitHub: any
+  github: any
 
   file: boolean
-  gitHubAvailable: boolean
+  githubAvailable: boolean
   nameAvailable: boolean
-  name: string
-  version: string
-  description: string
-  gitHubError: string
-  nameError: string
+  isLoading: boolean
 
   tags : string[]
 
-  constructor(private extensionService: ExtensionsService, private router: Router, private sanitizer: DomSanitizer, private cdRef: ChangeDetectorRef) {
+  fileReader = new FileReader();
+
+  constructor(private extensionService: ExtensionsService, private router: Router, private sanitizer: DomSanitizer, private cdRef: ChangeDetectorRef, private fb: FormBuilder) {
     this.formData = new FormData()
     this.tags = []
-    this.name = ''
-    this.version = ''
-    this.description = ''
-    this.gitHubAvailable = null
-    this.nameAvailable = null
+    this.errors = {}
+    this.extensionForm = this.fb.group({
+      name : '',
+      github : '',
+      version : '',
+      description : ''
+    })
   }
 
   ngOnInit() {
-    this.nameInput.valueChanges.pipe(debounceTime(200)).subscribe(name => {
-      this.nameAvailable = undefined
-      this.nameError = null
-      this.name = name
+    this.extensionForm.get('name').valueChanges.pipe(debounceTime(300)).subscribe(name => {
+      this.nameAvailable = null
+      this.errors.name = null
       this.checkName(name)
     })
-    this.gitHubInput.valueChanges.pipe(debounceTime(200)).subscribe(gitHub => {
-      this.gitHubAvailable = undefined
-      this.gitHub = null
-      this.gitHubError = null
+
+    this.extensionForm.get('github').valueChanges.pipe(debounceTime(300)).subscribe(github => {
+      this.githubAvailable = null
+      this.github = null
+      this.errors.github = null
       
-      this.checkGithub(gitHub)
+      this.checkGithub(github)
     })
-    this.versionInput.valueChanges.pipe(debounceTime(200)).subscribe(version => {
-      this.version = version
-    })
-    this.descriptionInput.valueChanges.pipe(debounceTime(1000)).subscribe(description => {
-      this.description = description
-    })
-    this.tagsInput.valueChanges.subscribe(tag =>{
-      if (/\s/.test(tag)) {
-        if(tag.length > 1){
-          this.addTag(tag)
-        }else{
-          this.tagsInputElmnt.nativeElement.value = ''
-        }
-      }
-    })
+
+  }
+
+  tagInput(e){
+    this.errors.tag = ''
+    if(e.code == 'Space' || e.code == 'Enter'){
+      const tag = e.target.value.replace(/\s/g, '')
+      this.tagsInputElmnt.nativeElement.value = ''
+
+      this.addTag(tag)
+    }
   }
 
   ngAfterViewInit() {
@@ -87,67 +85,73 @@ export class CreateComponent implements OnInit {
   }
 
   checkName(name){
-    if(name.length == 0){
-      this.nameAvailable = null    
-    }else if(name.length > 7){
-      this.extensionService.checkName(name).subscribe((available: boolean) => {
-        this.nameAvailable = available
-        if(!this.nameAvailable){
-          this.nameError = 'Name is unavailable.'
-        }else{
-          this.name = name
-          this.nameAvailable = true
-        }
-      })
-    }else{
-      this.nameAvailable = false
-      this.nameError = 'Name is less than 7 symbolls'
+    if(name.length < 7 || name.length > 30){
+      this.errors.name = 'Name must be between 7 and 30 characters.'
+      this.nameAvailable = false  
+      return  
     }
-  }
-
-  checkGithub(gitHub){
-    const pattern = /^https:\/\/github\.com\/.+\/.+$/
-    if(gitHub.length == 0){
-      this.gitHubAvailable = null
-    }else if(pattern.test(gitHub)){
-      this.extensionService.checkGithub(gitHub).subscribe(
-        gitHub => {
-          this.gitHub = gitHub
-          this.gitHubAvailable = true
-        },
-        error => {
-          this.gitHubAvailable = false
-          this.gitHubError = error
-        })
-    }else{
-      this.gitHubAvailable = false
-      this.gitHubError = 'Link is not a valid repo URL.'
-    }
-  }
-
-  createExtension(extensionForm : NgForm){
-    if(this.nameAvailable && this.gitHubAvailable){
-      const name = this.nameInput.value
-      const github = this.gitHubInput.value
-      const version = this.version
-      const description = this.description
-      const tags = this.tags.length > 0 ? this.tags.toString() : undefined 
-      this.gitHubAvailable = undefined
-
-      const extension = {
-        name,
-        version,
-        description,
-        github,
-        tags
+     
+    this.extensionService.checkName(name).subscribe((available: boolean) => {
+      this.nameAvailable = available
+      if(!available){
+        this.errors.name = 'Name is already in use.'
       }
-      this.formData.set('extension', JSON.stringify(extension))
-      
+    })
+  }
+
+  checkGithub(github){
+    const pattern = /^https:\/\/github\.com\/.+\/.+$/
+    if(!pattern.test(github)){
+      this.githubAvailable = false
+      this.errors.github = 'Link is not a valid github.'
+      return
+    }
+
+    this.extensionService.checkGithub(github).subscribe(
+      github => {
+        this.github = github
+        this.githubAvailable = true
+      },
+      error => {
+        this.githubAvailable = false
+        this.errors.github = error.error
+    })
+  }
+
+  createExtension(){
+    if(!this.errors.github && !this.errors.name){
+      const { name, github, version, description } = this.extensionForm.value
+
+      if(this.tags.length > 0) this.formData.set('tags', this.tags.toString()) 
+
+      this.formData.set('name', name)
+      this.formData.set('github', github)
+      this.formData.set('version', version)
+      this.formData.set('description', description)
+
+      this.isLoading = true
       this.extensionService.createExtension(this.formData).subscribe(
         data => {
-          this.gitHubAvailable = true
+          this.isLoading = false
+          this.githubAvailable = true
           this.extensionService.currentExtension = data
           this.router.navigate(['extension', data.id])
+        },
+        err => {
+          this.isLoading = false
+
+          if(err.error.name){
+            this.nameAvailable = false
+          }
+          if(err.error.github){
+            this.githubAvailable = false
+          }
+
+          if(err.status == 422){
+            this.errors = err.error
+          }else{
+            this.error = err.error
+          }
         })
     }
   }
@@ -156,22 +160,12 @@ export class CreateComponent implements OnInit {
     const logo = e.target.files[0]
     this.formData.set('image', logo)
     this.logoURL = window.URL.createObjectURL(logo)
-
-    let reader = new FileReader();
-    reader.readAsDataURL(logo); 
-    reader.onload = (_event) => { 
-      this.logoURL = reader.result; 
-    }
   }
 
   addCover(e){
     const cover = e.target.files[0]
     this.formData.set('cover', cover)
-    let reader = new FileReader();
-    reader.readAsDataURL(cover); 
-    reader.onload = (_event) => { 
-      this.coverURL = reader.result; 
-    }
+    this.coverURL = window.URL.createObjectURL(cover)
   }
 
   addFile(e){
@@ -184,20 +178,24 @@ export class CreateComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 
-  focusInput(e){
-    this.tagsInputElmnt.nativeElement.focus()
-  }
-
   addTag(tag){
-    tag = tag.trim()
-    this.tagsInputElmnt.nativeElement.value = ''
-    if(!this.tags.includes(tag)){
-      this.tags.push(tag)
-      const scrollHeight = this.tagsContainer.nativeElement.scrollHeight
-      const offsetHeight = this.tagsContainer.nativeElement.offsetHeight
-      if(scrollHeight > offsetHeight){
-        this.tags.pop()
-      }
+    if(tag.length == 0 || tag.length > 30){
+      this.errors.tag = `Tag can't be empty or more than 30 characters.`
+      return
+    }
+
+    if(this.tags.includes(tag)){
+      this.errors.tag = 'Tag already exists.'
+      return
+    }
+   
+    this.tags.push(tag)
+    const scrollHeight = this.tagsContainer.nativeElement.scrollHeight
+    const offsetHeight = this.tagsContainer.nativeElement.offsetHeight
+
+    if(scrollHeight > offsetHeight){
+      this.tags.pop()
+      this.errors.tag = 'You have reached maximum amount of tags.'
     }
   }
 
@@ -205,4 +203,12 @@ export class CreateComponent implements OnInit {
     const index = this.tags.indexOf(tag)
     this.tags.splice(index, 1)
   }
+}
+
+type FieldErrors = {
+  name? : string
+  version? : string
+  description? : string
+  github? : string
+  tag? : string
 }
