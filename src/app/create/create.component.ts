ab16d8d27b51babda835f5c2c6b897d0af0b7572
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-01 22:53:29 +0300

    create handleError + subscription + typed elementRef added

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms'
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { ExtensionsService } from '../services/extensions.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { SubmitScrollDirective } from '../helpers/submit-scroll.directive';
+import { Subject } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-create',
@@ -12,10 +14,10 @@ import { SubmitScrollDirective } from '../helpers/submit-scroll.directive';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  @ViewChild('tagsInputElmnt') tagsInputElmnt: ElementRef
-  @ViewChild('tagsContainer') tagsContainer: ElementRef
+  @ViewChild('tagsInputElmnt') tagsInputElmnt: ElementRef<HTMLInputElement>
+  @ViewChild('tagsContainer') tagsContainer: ElementRef<HTMLElement>
   @ViewChild(SubmitScrollDirective) wheelDirective: SubmitScrollDirective
-  @ViewChild('extensionSection') extensionSection: ElementRef
+  @ViewChild('extensionSection') extensionSection: ElementRef<HTMLElement>
 
   nameInput: FormControl = new FormControl()
   githubInput: FormControl = new FormControl()
@@ -38,6 +40,8 @@ export class CreateComponent implements OnInit {
   tags : string[]
 
   fileReader = new FileReader();
+  subscription: Subject<void> = new Subject<void>();
+
 
   constructor(private extensionService: ExtensionsService, private router: Router, private sanitizer: DomSanitizer, private cdRef: ChangeDetectorRef, private fb: FormBuilder) {
     this.formData = new FormData()
@@ -52,20 +56,26 @@ export class CreateComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.extensionForm.get('name').valueChanges.pipe(debounceTime(300)).subscribe(name => {
+    this.subscribeToName()
+    this.subscribeToGithub()
+  }
+
+  subscribeToName(){
+    this.extensionForm.get('name').valueChanges.pipe(debounceTime(300), takeUntil(this.subscription)).subscribe(name => {
       this.nameAvailable = null
       this.errors.name = null
       this.checkName(name)
     })
+  }
 
-    this.extensionForm.get('github').valueChanges.pipe(debounceTime(300)).subscribe(github => {
+  subscribeToGithub(){
+    this.extensionForm.get('github').valueChanges.pipe(debounceTime(300), takeUntil(this.subscription)).subscribe(github => {
       this.githubAvailable = null
       this.github = null
       this.errors.github = null
       
       this.checkGithub(github)
     })
-
   }
 
   tagInput(e){
@@ -80,18 +90,17 @@ export class CreateComponent implements OnInit {
 
   ngAfterViewInit() {
     this.wheelDirective.submitComponent.extensionSection = this.extensionSection
-    this.wheelDirective.checkIfMobileScreen()
     this.cdRef.detectChanges()
   }
 
-  checkName(name){
+  checkName(name : string){
     if(name.length < 7 || name.length > 30){
       this.errors.name = 'Name must be between 7 and 30 characters.'
       this.nameAvailable = false  
       return  
     }
      
-    this.extensionService.checkName(name).subscribe((available: boolean) => {
+    this.extensionService.checkName(name).pipe(takeUntil(this.subscription)).subscribe((available: boolean) => {
       this.nameAvailable = available
       if(!available){
         this.errors.name = 'Name is already in use.'
@@ -99,15 +108,15 @@ export class CreateComponent implements OnInit {
     })
   }
 
-  checkGithub(github){
+  checkGithub(githubLink : string){
     const pattern = /^https:\/\/github\.com\/.+\/.+$/
-    if(!pattern.test(github)){
+    if(!pattern.test(githubLink)){
       this.githubAvailable = false
       this.errors.github = 'Link is not a valid github.'
       return
     }
 
-    this.extensionService.checkGithub(github).subscribe(
+    this.extensionService.checkGithub(githubLink).pipe(takeUntil(this.subscription)).subscribe(
       github => {
         this.github = github
         this.githubAvailable = true
@@ -130,32 +139,36 @@ export class CreateComponent implements OnInit {
       this.formData.set('description', description)
 
       this.isLoading = true
-      this.extensionService.createExtension(this.formData).subscribe(
+      this.extensionService.createExtension(this.formData).pipe(takeUntil(this.subscription)).subscribe(
         data => {
           this.isLoading = false
           this.githubAvailable = true
           this.extensionService.currentExtension = data
           this.router.navigate(['extension', data.id])
         },
-        err => {
-          this.isLoading = false
-
-          if(err.error.name){
-            this.nameAvailable = false
-          }
-          if(err.error.github){
-            this.githubAvailable = false
-          }
-
-          if(err.status == 422){
-            this.errors = err.error
-          }else{
-            this.error = err.error
-          }
+        (err : HttpErrorResponse) => {
+          this.handleError(err)
         })
     }
   }
 
+  handleError(err : HttpErrorResponse){
+    this.isLoading = false
+
+    if(err.error.name){
+      this.nameAvailable = false
+    }
+    if(err.error.github){
+      this.githubAvailable = false
+    }
+
+    if(err.status == 422){
+      this.errors = err.error
+    }else{
+      this.error = err.error
+    }
+  }
+  
   addLogo(e){
     const logo = e.target.files[0]
     this.formData.set('image', logo)
@@ -178,7 +191,7 @@ export class CreateComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 
-  addTag(tag){
+  addTag(tag : string){
     if(tag.length == 0 || tag.length > 30){
       this.errors.tag = `Tag can't be empty or more than 30 characters.`
       return
@@ -199,7 +212,7 @@ export class CreateComponent implements OnInit {
     }
   }
 
-  removeTag(tag){
+  removeTag(tag : string){
     const index = this.tags.indexOf(tag)
     this.tags.splice(index, 1)
   }
