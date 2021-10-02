9c9890eddfc87fc6c297d74bb719907e288229e6
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-02 21:18:30 +0300

    tag extensions logic removed + subscriptions added

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewChildren, QueryList, HostListener, ViewChild } from '@angular/core';
 import { ExtensionsService, Extension } from '../services/extensions.service';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { MouseWheelDirective } from '../helpers/mouse-wheel.directive';
-import { Subscription } from 'rxjs';
+import { ExtensionScrollDirective } from '../helpers/extension-scroll.directive';
+import { Subject, Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tags',
@@ -11,99 +12,37 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['../pendings/pendings.component.css']
 })
 export class TagsComponent implements OnInit {
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.fixOverflow(this.extensionDescriptions)
-  }
+  @ViewChild(ExtensionScrollDirective) wheelDirective: ExtensionScrollDirective
 
-  @ViewChildren('extensionDescriptions') extensionDescriptions: QueryList<any>
-  @ViewChild(MouseWheelDirective) wheelDirective: MouseWheelDirective
-
-  totalExtensions: Extension[]
-  extensions: Extension[]
-  tag: string
-  routeSubscription: Subscription
-  baseUrl: string
-
-  config = {
-    itemsPerPage: 12,
-    currentPage: 1,
-    totalItems: 0
-  }
+  baseUrl : string
+  subscription : Subject<void> = new Subject<void>()
 
   constructor(private extensionsService: ExtensionsService, private route: ActivatedRoute, private router: Router) { 
+    this.extensionsService.config.itemsPerPage = 2
     this.baseUrl = environment.baseUrl
   }
 
-  ngOnInit() {
-    this.tag = this.route.snapshot.paramMap.get('tag')
-    this.findByTag(this.tag)
-  }
-
   ngOnDestroy() {
-    if (this.routeSubscription) {  
-      this.routeSubscription.unsubscribe();
-    }
+    this.subscription.next()
+    this.subscription.complete()
+    this.extensionsService.resetExtensions()
   }
 
   ngAfterViewInit() {
-    this.extensionDescriptions.changes.subscribe(descriptions => {
-      this.fixOverflow(descriptions.toArray())
-    })
+    this.subscribeToRoute()
+    this.extensionsService.nameQuery = this.route.snapshot.paramMap.get('tag')
+    this.extensionsService.getByTag(this.subscription)
+  }
 
-    this.routeSubscription = this.router.events.subscribe((e: any) => {
+  ngOnInit() {
+  }
+  
+  subscribeToRoute(){
+    this.router.events.pipe(takeUntil(this.subscription)).subscribe((e: any) => {
       if (e instanceof NavigationEnd) {
-        this.tag = this.route.snapshot.paramMap.get('tag')
-        this.findByTag(this.tag)
+        this.extensionsService.nameQuery = this.route.snapshot.paramMap.get('tag')
+        this.extensionsService.getByTag(this.subscription)
       }
     })
-  }
-
-  findByTag(tag: string){
-    this.extensionsService.findByTag(tag, this.config.itemsPerPage).subscribe(page =>{
-      this.extensions = page.data
-      this.totalExtensions = page.data
-      this.config.totalItems = page.totalResults
-    })
-  }
-
-  fixOverflow(descriptions){
-    descriptions.forEach((description, i) => {
-      description.nativeElement.innerHTML = this.extensions[i].description
-      
-      let height = description.nativeElement.offsetHeight
-      let scrollHeight = description.nativeElement.scrollHeight
-      let text = description.nativeElement.innerHTML + '...'
-    
-      while(height < scrollHeight){
-        let words = text.split(' ')
-        words.pop()
-        words.pop()
-        text = words.join(' ') + '...'
-        
-        description.nativeElement.innerHTML = text
-        height = description.nativeElement.offsetHeight
-        scrollHeight = description.nativeElement.scrollHeight
-      }
-    })
-  }
-
-  changePage(page){
-    const length = this.totalExtensions.length
-    const itemsPerPage = this.config.itemsPerPage;
-
-    if(length <= (page - 1) * itemsPerPage){
-      this.extensionsService.findByTag(this.tag, itemsPerPage * (page - length / itemsPerPage), this.totalExtensions[length - 1].id).subscribe(pageData => {
-        this.config.totalItems = this.totalExtensions.length + pageData.totalResults
-        this.totalExtensions.push(...pageData.data)
-        this.extensions = this.totalExtensions.slice((page - 1) * itemsPerPage, page * itemsPerPage)
-        this.config.currentPage = page;
-        this.wheelDirective.calculateScrollAmount()
-      })
-    }else{
-      this.extensions = this.totalExtensions.slice((page - 1) * itemsPerPage, page * itemsPerPage);
-      this.config.currentPage = page;
-      this.wheelDirective.calculateScrollAmount()
-    }
   }
 }
