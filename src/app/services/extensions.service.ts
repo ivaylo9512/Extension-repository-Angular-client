8a8bf1add1d3be706542c9c12681d90a971a7dc0
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-02 23:11:18 +0300

    types added + pagination logic

--- src/app/services/extensions.service.ts
+++ src/app/services/extensions.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-export interface Extension {
+export type Extension = {
   id: number,
   name: string,
   version: string,
@@ -23,21 +24,31 @@ export interface Extension {
   github: Github
 }
 
-export interface Github {
-  id : number;
-  pullRequests : number;
-  openIssues : number;
-  user : string;
-  repo : string;
-  failMessage : string;
-  lastCommit : string;
-  lastSuccess : string;
-  lastFail : string;
+export type Github = {
+  id : number
+  pullRequests : number
+  openIssues : number
+  user : string
+  repo : string
+  failMessage : string
+  lastCommit : string
+  lastSuccess : string
+  lastFail : string
 }
 
-interface Page {
+type Page = {
   data: Extension[],
   totalResults: number
+}
+
+type PageConfig = {
+  itemsPerPage: number,
+  currentPage: number,
+  page? : number,
+  nextPage? : number,
+  subscription? : Subject<void>,
+  totalItems?: number,
+  isLoading : boolean
 }
 
 @Injectable({
@@ -50,128 +61,145 @@ export class ExtensionsService {
   totalExtensions : Extension[]
   currentExtensions : Extension[]
   nameQuery : string
+  orderBy : string
   component : string
 
-  config = {
+  config : PageConfig= {
     itemsPerPage: 8,
     currentPage: 1,
-    totalItems: null
+    totalItems: null,
+    nextPage: null,
+    subscription: null,
+    isLoading: false
   }
   
   constructor(private httpClient : HttpClient) { 
   }
 
-  getNextExtensions(page : number){
-    switch(this.component){
-      case 'ProfileComponent' :
-      case 'HomeComponent' :
-        this.getNextUserExtensions(page)
-      case 'PendingsComponent' :
-        this.getNextPending
-    }
+  getNextExtensions(page : number, subscription : Subject<void>){
+    this.config.nextPage = page
+    this.config.subscription = subscription
+  
+    let request = (() : (pageSize : number) => Observable<Page> => { switch(this.component){
+      case 'ProfileComponent' : return this.findUserExtensions
+      case 'HomeComponent' : return this.findUserExtensions
+      case 'PendingsComponent' : return this.findAllByPendings
+      case 'TagsComponent' : return this.findAllByTag
+      case 'DiscoverComponent' : return this.decideAllOrderBy()
+    }})()
+    
+    this.onPageChange(request)
   }
 
   resetExtensions() {
-    this.config.currentPage = 1
-    this.config.totalItems = 0
-    this.totalExtensions = null
-    this.currentExtensions = null
+    this.config.currentPage = null
+    this.config.totalItems = null
+    this.totalExtensions = []
+    this.currentExtensions = []
     this.nameQuery = null
+    this.config.isLoading = false
   }
 
-  getUserExtensions(){
-    this.findUserExtensions(this.config.itemsPerPage).subscribe(page => {
+  getUserExtensions(subscription : Subject<void>){
+    this.findUserExtensions(this.config.itemsPerPage).pipe(takeUntil(subscription)).subscribe(page => {
       this.totalExtensions = this.currentExtensions = page.data
       this.config.totalItems = page.totalResults
     })
   }
 
-  getPending() {
-    this.findPendings(this.config.itemsPerPage).subscribe(page => {
+  getAll(subscription : Subject<void>){
+    this.decideAllOrderBy()(this.config.itemsPerPage).pipe(takeUntil(subscription)).subscribe(page => {
       this.totalExtensions = this.currentExtensions = page.data
       this.config.totalItems = page.totalResults
     })
   }
 
-  getNextPending(page: number){
-    const length = this.totalExtensions.length
-    const itemsPerPage = this.config.itemsPerPage;
-    this.onPageChange(this.findPendings(itemsPerPage * (page - length / itemsPerPage), this.totalExtensions[length - 1].id), page);
+  getPending(subscription : Subject<void>) {
+    this.findAllByPendings(this.config.itemsPerPage).pipe(takeUntil(subscription)).subscribe(page => {
+      this.totalExtensions = this.currentExtensions = page.data
+      this.config.totalItems = page.totalResults
+    })
   }
 
-  getNextUserExtensions(page : number){
-    const length = this.totalExtensions.length
-    const itemsPerPage = this.config.itemsPerPage;
-    this.onPageChange(this.findUserExtensions(itemsPerPage * (page - length / itemsPerPage), this.totalExtensions[length - 1].id), page)
+  getByTag(subscription : Subject<void>){
+    this.findAllByTag(this.config.itemsPerPage).pipe(takeUntil(subscription)).subscribe(page => {
+      this.totalExtensions = this.currentExtensions = page.data
+      this.config.totalItems = page.totalResults
+    })
   }
 
-  onPageChange(request : Observable<Page>, page : number) {
-    const length = this.totalExtensions.length
-    const itemsPerPage = this.config.itemsPerPage;
-
-    if(length <= (page - 1) * itemsPerPage){
-      request.subscribe(pageData => {
-        this.setExtensions(pageData, page)
-      })
-    }else{
-      this.currentExtensions = this.totalExtensions.slice((page - 1) * itemsPerPage, page * itemsPerPage);
-      this.config.currentPage = page;
+  decideAllOrderBy = () : (pageSize : number) => Observable<Page> => {
+    switch(this.orderBy){
+      case 'name' : return this.findAllByName
+      case 'commits' : return this.findAllByCommitDate
+      case 'downloads' : return this.findAllByDownloads
+      case 'date' : return this.findAllByUploadDate
     }
   }
 
-  setExtensions(pageData : Page, page : number){
+  onPageChange(request : (pageSize : number) => Observable<Page>) {
+    const page = this.config.nextPage
+    const length = this.totalExtensions.length
+    const itemsPerPage = this.config.itemsPerPage
+
+    if(length <= (page - 1) * itemsPerPage){
+      this.config.isLoading = true
+      this.currentExtensions = []
+      request(itemsPerPage * (page - length / itemsPerPage)).pipe(takeUntil(this.config.subscription)).subscribe(pageData => {
+        this.setExtensions(pageData)
+      })
+    }else{
+      this.currentExtensions = this.totalExtensions.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+      this.config.currentPage = page
+    }
+  }
+
+  setExtensions(pageData : Page){
+    const page = this.config.nextPage
+
     this.config.totalItems = this.totalExtensions.length + pageData.totalResults
     this.totalExtensions.push(...pageData.data)
     this.currentExtensions = this.totalExtensions.slice((page - 1) * this.config.itemsPerPage, page * this.config.itemsPerPage)
-    this.config.currentPage = page;
+    
+    this.config.currentPage = page
+    this.config.isLoading = false
   }
 
-  findUserExtensions(pageSize : number, lastId? : number){
-    return this.httpClient.get<Page>(`/api/extensions/auth/findUserExtensions/${pageSize}/${lastId ? lastId : ''}`)
+    return this.httpClient.get<Page>('/api/extensions/findAllByDownloadCount', { params })
   }
-  findPendings(pageSize : number, lastId? : number){
-    return this.httpClient.get<Page>(`/api/extensions/auth/findPending/true/${pageSize}/${lastId ? lastId : ''}`)
-  }
-  findByTag(name : string, pageSize : number, lastId? : number){
-    return this.httpClient.get<any>(`/api/extensions/findByTag/${name}/${pageSize}/${lastId ? lastId : ''}`)
-  }
-  getFeatured(){
+  getFeatured() : Observable<Extension[]>{
     return this.httpClient.get<Extension[]>('/api/extensions/featured')
   }
-  getExtension(id : number){
+  getExtension(id : number) : Observable<Extension>{
     return this.httpClient.get<Extension>(`/api/extensions/${id}`)
   }
-  editExtension(id : number, formData){
-    return this.httpClient.post<Extension>(`/api/extensions/auth/edit`, formData)
+  editExtension(id : number, extensionForm : FormData) : Observable<Extension>{
+    return this.httpClient.post<Extension>(`/api/extensions/auth/edit`, extensionForm)
   }
-  createExtension(formData){
-    return this.httpClient.post<Extension>('/api/extensions/auth/create', formData)
+  createExtension(extensionForm : FormData) : Observable<Extension> {
+    return this.httpClient.post<Extension>('/api/extensions/auth/create', extensionForm)
   }
-  checkName(name : string){
+  checkName(name : string) : Observable<boolean>{
     const params = new HttpParams().set('name', name)
-    return this.httpClient.get('/api/extensions/checkName', {params})
+    return this.httpClient.get<boolean>('/api/extensions/checkName', {params})
   }
   checkGithub(gitHub : string){
     const params = new HttpParams().set('link', gitHub)
     return this.httpClient.get('/api/github/getRepoDetails', {params})
   }
-  findExtensions(name : string, criteria : string, page : string, perPage : string){
-    const params = new  HttpParams().set('name', name).set('orderBy', criteria).set('page', page).set('perPage', perPage)
-    return this.httpClient.get<any>('/api/extensions/filter', {params})                                          
-  }
-  setFeatured(id : number, state : boolean){
+  setFeatured(id : number, state : boolean) : Observable<Extension>{
     return this.httpClient.patch<Extension>(`/api/extensions/auth/${id}/featured/${state}`, null)
   }
-  setPending(id : number, state : boolean){
+  setPending(id : number, state : boolean) : Observable<Extension>{
     return this.httpClient.patch<Extension>(`/api/extensions/auth/${id}/status/${state}`, null)
   }
   deleteExtension(id : number){
     return this.httpClient.delete(`/api/extensions/auth/${id}`)
   }
-  refreshGitHub(id : number){
+  refreshGitHub(id : number) : Observable<Github> {
     return this.httpClient.patch<Github>(`/api/github/auth/${id}/fetch`,null)
   }
-  rateExtension(id : number, rating : number){
+  rateExtension(id : number, rating : number) : Observable<number> {
     return this.httpClient.patch<number>(`/api/rating/auth/rate/${id}/${rating}`, {})
   }
 }
